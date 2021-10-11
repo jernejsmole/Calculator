afedfd5af0e100d740799c3dbb286c2d5d164a44
jernejsmole <jersmole@gmail.com>
2021-10-11 09:17:28 +0200

    calc 9.10.21 morgen

---
+++ src/arrayDemo.java
@@ -0,0 +1,18 @@
+public class arrayDemo {
+    public static void main(String[] args) {
+        double array1[]=new double[3];
+            array1[1] = 1;
+            array1[2] = 2;
+            array1[3] = 3;
+
+
+        double array2[]=new double[3];
+            array2[1] = 4;
+            array2[2] = 5;
+            array2[3] = 6;
+
+            int sum = 0;
+            int count = 0;
+            for (int sum = 1; )
+    }
+}
